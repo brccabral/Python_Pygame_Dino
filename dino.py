871781f657deb223ce680042d2d624a57b273519
Bruno Cabral <cabral.brc@gmail.com>
2022-05-03 10:27:23 -0700

    show background

--- dino.py
+++ dino.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import pygame
 
@@ -10,7 +11,30 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Dino")
 
 
+class BG:
+    def __init__(self):
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.set_texture()
+        self.show()
+
+    def set_texture(self):
+        path = os.path.join("assets", "images", "bg.png")
+        self.texture = pygame.image.load(path).convert()
+        self.texture = pygame.transform.scale(self.texture, (self.width, self.height))
+
+    def show(self):
+        screen.blit(self.texture, (0, 0))
+
+
+class Game:
+    def __init__(self):
+        self.gb = BG()
+
+
 def main():
+    game = Game()
+
     while True:
         for event in pygame.event.get([pygame.QUIT]):
             if event.type == pygame.QUIT:
