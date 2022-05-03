ef71f645fa0f6a869ffb5fec9673c595de072f0e
Bruno Cabral <cabral.brc@gmail.com>
2022-05-03 10:33:13 -0700

    background moves

--- dino.py
+++ dino.py
@@ -9,14 +9,20 @@ HEIGHT = 150
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Dino")
+clock = pygame.time.Clock()
 
 
 class BG:
-    def __init__(self):
+    def __init__(self, x: int, y: int):
         self.width = WIDTH
         self.height = HEIGHT
+        self.x = x
+        self.y = y
         self.set_texture()
         self.show()
+
+    def update(self, dx: float):
+        self.x += dx
 
     def set_texture(self):
         path = os.path.join("assets", "images", "bg.png")
@@ -24,24 +30,31 @@ class BG:
         self.texture = pygame.transform.scale(self.texture, (self.width, self.height))
 
     def show(self):
-        screen.blit(self.texture, (0, 0))
+        screen.blit(self.texture, (self.x, self.y))
 
 
 class Game:
     def __init__(self):
-        self.gb = BG()
+        self.bg = BG(0, 0)
+        self.speed = 60
 
 
 def main():
     game = Game()
+    dt = 0
 
     while True:
+        screen.fill("black")
+        game.bg.update(-game.speed * dt)
+        game.bg.show()
+
         for event in pygame.event.get([pygame.QUIT]):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
         pygame.display.update()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
